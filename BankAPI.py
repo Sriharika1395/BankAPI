d20498b754aef335b1b4b696f6243d778f72869f
Sriharika <Sriharika@Sriharikas-MacBook-Pro.local>
2018-12-04 22:50:44 +0530

    Updated readme file

--- BankAPI.py
+++ BankAPI.py
@@ -10,30 +10,36 @@ mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 
 @app.route("/login/<accountid>/<password>")
-def Login(accountid,password):
-    account_info1 = mongo.db.users.find({"AccountID": accountid1})
-    password_hash = account_info1['password']
-    if bcrypt.check_password_hash(password_hash, password):
-        return "Valid Credentials"
-    else:
-        return "Invalid Credentials"
+def login(accountid,password):
+    try:
+        account_info1 = mongo.db.users.find({"AccountID": accountid})
+        password_hash = account_info1['password']
+        if bcrypt.check_password_hash(password_hash, password):
+            return "Valid Credentials"
+        else:
+            return "Invalid Credentials"
+    except:
+        return "Unable to Fetch Details"
 
-@app.route("/getInfo/<accountnum>")
-def TransactionSummary(accountname):
-    account_info = mongo.db.users.find({"AccountID": accountnum})
-    amount = account_info['Balance']
-    return str(amount)
+@app.route("/getbalance/<accountid>")
+def getBalance(accountid):
+    try:
+        account_info = mongo.db.users.find({"AccountID": accountid})
+        amount = account_info['Balance']
+        return str(amount)
+    except:
+        return "Unable to Flasketch Details"
     
 @app.route("/addBeneficiary/<accountid>/<customername>/<balance>")
-def AddBeneficiary(accountid,customername,balance):
+def addBeneficiary(accountid,customername,balance):
     try:
-        account_info = mongo.db.users.insert({"AccountID": accountid,"CustomerName":accountname,"Balance":balance})
+        account_info = mongo.db.users.insert({"AccountID": accountid,"CustomerName":customername,"Balance":balance})
         return "Beneficiary Added"
     except Exception as e:
         return "Unable to Add Beneficiary"
         
 @app.route("/deleteBeneficiary/<accountid>")
-def DeleteBeneficiary(accountid):
+def deleteBeneficiary(accountid):
     try:
         account_info = mongo.db.users.remove({"AccountID": accountid})
         return "Account deleted"
@@ -41,7 +47,7 @@ def DeleteBeneficiary(accountid):
         return "Unable to Delete the account"
         
 @app.route("/transferFunds/<accountid1>/<accountid2>/<amount>")
-def TransferFunds(accountid1,accountid2,amount):
+def transferFunds(accountid1,accountid2,amount):
     try:
         account_info1 = mongo.db.users.find({"AccountID": accountid1})
         account_info1 = mongo.db.users.find({"AccountID": accountid2})
@@ -56,18 +62,21 @@ def TransferFunds(accountid1,accountid2,amount):
             account_info1 = mongo.db.users.update({"AccountID": accountid2},{$set:{"Balance":balance2}})
             
     except:
-        return "Unable to fetch Account Details"
+        return "Unable to fetch  Details"
         
 @app.route("/futureAmount/<accountid>/<futuredate>")
-def FutureAmount(accountid,futuredate):
-    currentdate = datetime.date.today()
-    delta = currentdate - futuredate
-    noOfDays = delta.days
-    account_info = mongo.db.users.find({"AccountID": accountid})
-    balance = account_info['Balance']
-    intrest = (balance*noOfDays*4)/(100*365)
-    total_amount = balance+intrest
-    return str(total_amount)
+def futureAmount(accountid,futuredate):
+    try:
+        currentdate = datetime.date.today()
+        delta = currentdate - futuredate
+        noOfDays = delta.days
+        account_info = mongo.db.users.find({"AccountID": accountid})
+        balance = account_info['Balance']
+        interest = (balance*noOfDays*4)/(100*365)
+        total_amount = balance+interest
+        return str(total_amount)
+    except:
+        return "Unable to fetch Details"
     
 
     
