d64662806ec9c4a026bdd4a3677f2ff596f6f259
Sriharika <Sriharika@Sriharikas-MacBook-Pro.local>
2018-12-05 15:42:23 +0530

    Modified and Tested all functions except Login

--- BankAPI.py
+++ BankAPI.py
@@ -2,13 +2,15 @@ import flask
 from flask import Flask
 from flask_pymongo import PyMongo
 from datetime import date
-from flask.ext.bcrypt import Bcrypt
+#from flask.ext.bcrypt import Bcrypt
+#import Bcrypt
 
 app = Flask(__name__)
 app.config["MONGO_URI"]="mongodb://localhost:27017/BankDatabase"
 mongo = PyMongo(app)
-bcrypt = Bcrypt(app)
+#bcrypt = Bcrypt(app)
 
+"""
 @app.route("/login/<accountid>/<password>")
 def login(accountid,password):
     try:
@@ -20,91 +22,92 @@ def login(accountid,password):
             return "Invalid Credentials"
     except:
         return "Unable to Fetch Details"
+"""
 
 @app.route("/getbalance/<accountid>")
 def getBalance(accountid):
     try:
+        accountid = int(accountid)
         account_info = mongo.db.users.find({"AccountID": accountid})
-        amount = account_info['Balance']
-        return str(amount)
-    except:
-        return "Unable to Flasketch Details"
+        if account_info.count() > 0:
+            for record in account_info:
+                amount = record['Balance']
+                return "Available Balance of Account Number "+str(accountid)+" is Rs "+str(amount)
+        else:
+            return "Customer Information doesnot exist"
+    except Exception as e:
+        return "Unable to Fetch Details",e
     
 @app.route("/addBeneficiary/<accountid>/<customername>/<balance>")
 def addBeneficiary(accountid,customername,balance):
     try:
+        accountid = int(accountid)
+        balance = int(balance)
         account_info = mongo.db.users.insert({"AccountID": accountid,"CustomerName":customername,"Balance":balance})
-        return "Beneficiary Added"
+        return "Beneficiary Added: "+str(customername)
     except Exception as e:
         return "Unable to Add Beneficiary"
         
 @app.route("/deleteBeneficiary/<accountid>")
 def deleteBeneficiary(accountid):
     try:
-        account_info = mongo.db.users.remove({"AccountID": accountid})
-        return "Account deleted"
+        accountid = int(accountid)
+        account_info = mongo.db.users.find({"AccountID": accountid})
+        if account_info.count() > 0:
+            account_info = mongo.db.users.remove({"AccountID": accountid})
+            return "Account Deleted: "+str(accountid)
+        else:
+            return "Customer Information doesnot exist"
     except Exception as e:
         return "Unable to Delete the account"
-        
+     
 @app.route("/transferFunds/<accountid1>/<accountid2>/<amount>")
 def transferFunds(accountid1,accountid2,amount):
     try:
+        accountid1 = int(accountid1)
+        accountid2 = int(accountid2)
+        amount = int(amount)
         account_info1 = mongo.db.users.find({"AccountID": accountid1})
-        account_info1 = mongo.db.users.find({"AccountID": accountid2})
-        balance1 = account_info['Balance']
-        balance2 = account_info['Balance']
-        if(balance1<amount):
-            return "No Sufficient Funds"
-        else:
-            balance2 += amount
-            balance1 -= amount
-            account_info1 = mongo.db.users.update({"AccountID": accountid1},{$set:{"Balance":balance1}})
-            account_info1 = mongo.db.users.update({"AccountID": accountid2},{$set:{"Balance":balance2}})
-            
+        account_info2 = mongo.db.users.find({"AccountID": accountid2})
+        if account_info1.count() > 0 and account_info2.count() > 0:
+            for record in account_info1:
+                balance1 = int(record['Balance'])
+            for record in account_info2:
+                balance2 = int(record['Balance'])
+            if(balance1<amount):
+                return "No Sufficient Funds in :"+str(accountid1)
+            else:
+                balance2 += amount
+                balance1 -= amount
+                mongo.db.users.update({'AccountID': accountid1}, {'$set': {"Balance": balance1}}, multi=True)
+                mongo.db.users.update({'AccountID': accountid2}, {'$set': {"Balance": balance2}}, multi=True)
+                return "Transferred Funds"
     except:
         return "Unable to fetch  Details"
         
-@app.route("/futureAmount/<accountid>/<futuredate>")
-def futureAmount(accountid,futuredate):
+@app.route("/futureAmount/<accountid>/<day>/<month>/<year>")
+def futureAmount(accountid,day,month,year):
     try:
-        currentdate = datetime.date.today()
+        currentdate = date.today()
+        accountid = int(accountid)
+        day = int(day)
+        month = int(month)
+        year = int(year)
+        futuredate = date(year,month,day)
         delta = currentdate - futuredate
         noOfDays = delta.days
         account_info = mongo.db.users.find({"AccountID": accountid})
-        balance = account_info['Balance']
-        interest = (balance*noOfDays*4)/(100*365)
-        total_amount = balance+interest
-        return str(total_amount)
-    except:
-        return "Unable to fetch Details"
+        if account_info.count() > 0:
+            for record in account_info:
+                balance = record['Balance']
+            interest = (balance*noOfDays*4)/(100*365)
+            total_amount = balance+interest
+            return "Expected Balance of "+str(accountid)+" on "+str(futuredate)+" is Rs "+str(total_amount)
+        else:
+            "Customer Information doesnot exist"
+    except Exception as e:
+        return "Unable to fetch Details",e
     
 
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     
